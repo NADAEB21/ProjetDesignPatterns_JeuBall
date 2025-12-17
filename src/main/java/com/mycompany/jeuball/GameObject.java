da774d10e293770dd4558af715382142dcf4af0e
NADAEB21 <nadaebdelli4@gmail.com>
2025-12-17 16:00:55 +0100

    feat: create abstract class GameObject

---
+++ src/main/java/com/mycompany/jeuball/GameObject.java
@@ -0,0 +1,15 @@
+package com.mycompany.jeuball;
+
+//import statements
+import java.awt.Graphics;
+
+/**
+ * Classe abstraite représentant un objet de jeu.
+ * 
+ * @author Nada Ebdelli
+ */
+
+public abstract class GameObject {
+    public abstract void draw(Graphics g); // Afficher l'objet
+
+}
